1e454adb8b8d9d69caf0abfbf3a30ffece1f56db
banjo7331 <cylwikbartosz@gmail.com>
2024-11-10 13:32:08 +0100

    Added validator for Profile Command and error message for valid data.

--- src/Application/Common/Consts/ValidationError.cs
+++ src/Application/Common/Consts/ValidationError.cs
@@ -4,4 +4,6 @@ public static class ValidationError
 {
 	public const string NotEmpty = "Field cannot be empty";
 	public const string AuthorizationFailed = "AuthorizationError";
+	public const string NotValid = "Field is not valid";
+
 }
