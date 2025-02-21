3e394eed955047d4a844613395a900286b68b3aa
03albert09 <03albert09@gmail.com>
2025-02-21 18:01:33 +0100

    add create affiliation

---
+++ src/Application/Affiliations/CreateAffiliation/CreateAffiliationCommand.cs
@@ -0,0 +1,31 @@
+using Application.Common.Services;
+using AutoMapper;
+using Domain.Entities;
+using Domain.Enums;
+using Domain.Repositories;
+using Domain.Shared;
+using MediatR;
+
+namespace Application.Affiliations.CreateAffiliation;
+
+public record CreateAffiliationCommand(CreateAffiliationModel Affiliation) : IRequest<ICommandResult<CreateAffiliationResponse>>;
+
+internal class CreateAffiliationCommandHandler(IAuthenticationService authenticationService,IAffiliationRepository affiliationRepository,IMapper mapper,IUnitOfWork unitOfWork) 
+	: IRequestHandler<CreateAffiliationCommand, ICommandResult<CreateAffiliationResponse>>
+{
+	public async Task<ICommandResult<CreateAffiliationResponse>> Handle(CreateAffiliationCommand request, CancellationToken cancellationToken)
+	{
+		var user = await authenticationService.GetCurrentUser();
+		if (user is null || user.IsProfileSetUp is false)
+			return CommandResult.Failure<CreateAffiliationResponse>(ErrorResult.AuthorizationError);
+		
+		var affiliation = mapper.Map<Affiliation>(request.Affiliation);
+
+		affiliation.UserId=user.Id;
+
+		affiliationRepository.Add(affiliation, cancellationToken);
+		await unitOfWork.SaveChangesAsync(cancellationToken);
+		
+		return CommandResult.Success(new CreateAffiliationResponse());
+	}
+}
