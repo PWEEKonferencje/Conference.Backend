0c65d2a2cf4f79f12faba98c7c850c5780283dd8
banjo7331 <cylwikbartosz@gmail.com>
2025-03-26 16:50:30 +0100

    "add: added validator for orcid checksum"

--- src/Application/Profiles/SetProfileOrcid/SetProfileOrcidCommandValidator.cs
+++ src/Application/Profiles/SetProfileOrcid/SetProfileOrcidCommandValidator.cs
@@ -10,9 +10,42 @@ public class SetProfileOrcidCommandValidator : AbstractValidator<SetProfileOrcid
 	{
 		RuleFor(x => x.OrcidId)
 			.NotEmpty().WithMessage(ValidationError.NotEmpty)
-			.Matches(@"\d{16}").WithMessage(ValidationError.InvalidFormat)
+			.Matches(@"\d{15}[\dX]").WithMessage(ValidationError.InvalidFormat)
+			.Must(IsValidOrcid).WithMessage("Invalid ORCID checksum")
 			.MustAsync(async (_, _) =>
 				(await userManager.GetCurrentIdentity())?.UserProfileId is null)
 			.WithMessage(ValidationError.AlreadySet);
+	}
+	
+	private bool IsValidOrcid(string orcid)
+	{
+		if (string.IsNullOrWhiteSpace(orcid) || orcid.Length != 16)
+			return false;
+
+		if (!TryCalculateChecksumBase(orcid, out int checksumBase))
+			return false;
+
+		char expectedCheckDigit = CalculateCheckDigit(checksumBase);
+		return orcid[15] == expectedCheckDigit;
+	}
+
+	private bool TryCalculateChecksumBase(string orcid, out int checksumBase)
+	{
+		checksumBase = 0;
+		string first15Digits = orcid.Substring(0, 15);
+		foreach (char digit in first15Digits)
+		{
+			if (!char.IsDigit(digit))
+				return false;
+			checksumBase = (checksumBase + (digit - '0')) * 2;
+		}
+		return true;
+	}
+
+	private char CalculateCheckDigit(int checksumBase)
+	{
+		int remainder = checksumBase % 11;
+		int checkValue = (12 - remainder) % 11;
+		return checkValue == 10 ? 'X' : (char)('0' + checkValue);
 	}
 }
