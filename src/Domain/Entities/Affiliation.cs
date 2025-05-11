7a3741fdec5d14268fafda7d94c62c7daa6ca595
03albert09 <03albert09@gmail.com>
2025-05-11 15:50:56 +0200

    remove Description from affiliation entity

--- src/Domain/Entities/Affiliation.cs
+++ src/Domain/Entities/Affiliation.cs
@@ -5,7 +5,6 @@ public class Affiliation
 	public Guid Id { get; set; }
 	public string Workplace { get; set; } = default!;
 	public string Position { get; set; } = default!;
-	public string? Description { get; set; }
 	public bool IsAcademic { get; set; }
 	public int UserId { get; set; }
 	public User User { get; set; } 
