bfa7fbf09ed6ce5de3ca6def4dfc88e38805b5c8
03albert09 <03albert09@gmail.com>
2025-05-12 12:09:55 +0200

    add migratios

---
+++ src/Infrastructure/Migrations/20250512100550_RemoveFieldDescription.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveFieldDescription : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Affiliations_UserProfiles_UserId",
+                table: "Affiliations");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Affiliations");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Affiliations",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Affiliations_UserProfiles_UserId",
+                table: "Affiliations",
+                column: "UserId",
+                principalTable: "UserProfiles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Affiliations_UserProfiles_UserId",
+                table: "Affiliations");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Affiliations",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Affiliations",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Affiliations_UserProfiles_UserId",
+                table: "Affiliations",
+                column: "UserId",
+                principalTable: "UserProfiles",
+                principalColumn: "Id");
+        }
+    }
+}
